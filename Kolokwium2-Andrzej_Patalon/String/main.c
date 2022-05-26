e1a6586d0838879d7b4c62430ff3150163232ad9
„JerzyDarzyiOparzy” <>
2022-05-26 16:14:20 +0200

    commit3

---
+++ Kolokwium2-Andrzej_Patalon/String/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Napisz wskaŸnikow¹ wersjê funkcji strcpyEven. Funkcja ta ma kopiowaæ co drugi znak z napisu s do napisu t. Zadbaj o koniec napisu - NUL.
+char* strcpyEven(char *t, const char *s);
+
+int main(void)
+{
+    char* kopia = calloc(10, sizeof(char));
+    char napis[] = "abcdefghij";
+
+    strcpyEven(kopia, napis);
+
+    printf("%s", kopia);
+}
+
+char* strcpyEven(char *t, const char *s)
+{
+    char* temp = t;
+
+    while (*s)
+    {
+        *t = *s;
+        ++t;
+        s += 2;
+    }
+
+    *t = '\0';
+
+    return t;
+}
